6722d2f3e3c87672773cf460dbbf212e26e70a3b
gfouz <gfouz1975@gmail.com>
2022-06-02 19:58:33 +0200

    Updates

---
+++ assets/index.09e63cb0.js
@@ -0,0 +1,6 @@
+import{u as t,r as c,j as e,F as i,a as r,X as d,s as h}from"./index.c49b2943.js";import{B as a,D as u,M as m,a as f,b as C,c as p,d as M,H as S,e as v}from"./Header.1603638e.js";function g(){const{isOpen:l,onOpen:o,onClose:n}=t(),s=c.exports.useRef(null);return e(i,{children:r(y,{children:[r("div",{className:"main",children:[e(a,{m:"1em",ref:s,variant:"ghost",onClick:o,children:e("strong",{style:{color:"#ff0000"},children:l?"cerrar":"abrir"})}),e(d,{children:"CONSULTAS"})]}),r(u,{isOpen:l,placement:"left",onClose:n,children:[e(m,{}),r(f,{children:[e(C,{}),e(p,{style:{color:"#DC143C"},children:"PKI-CONSULTAS"}),e(M,{children:e(S,{color:"#800080",column:!0})}),r(v,{children:[e(a,{variant:"outline",mr:3,onClick:n,children:"Cancel"}),e(a,{colorScheme:"purple",children:"Save"})]})]})]})]})})}const y=h.div`
+  
+}
+  
+  
+`;export{g as default};
--- assets/index.f61e8522.js
+++
@@ -1,6 +0,0 @@
-import{u as t,r as c,j as e,F as i,a as r,X as d,s as h}from"./index.8b08a318.js";import{B as a,D as u,M as m,a as C,b as f,c as p,d as M,H as S,e as v}from"./Header.81665170.js";function O(){const{isOpen:n,onOpen:o,onClose:l}=t(),s=c.exports.useRef(null);return e(i,{children:r(y,{children:[r("div",{className:"main",children:[e(a,{m:"1em",ref:s,variant:"ghost",onClick:o,children:e("strong",{style:{color:"#ff0000"},children:n?"cerrar":"abrir"})}),e(d,{children:"CONSULTAS"})]}),r(u,{isOpen:n,placement:"right",onClose:l,children:[e(m,{}),r(C,{children:[e(f,{}),e(p,{style:{color:"#DC143C"},children:"PKI-CONSULTAS"}),e(M,{children:e(S,{color:"#800080",column:!0})}),r(v,{children:[e(a,{variant:"outline",mr:3,onClick:l,children:"Cancel"}),e(a,{colorScheme:"purple",children:"Save"})]})]})]})]})})}const y=h.div`
-  
-}
-  
-  
-`;export{O as default};
