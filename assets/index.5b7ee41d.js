fd5ca5900c78a6d0e44435aeb9232eceb3ddf296
gfouz <gfouz1975@gmail.com>
2022-06-15 06:32:46 +0200

    Updates

---
+++ assets/index.5b7ee41d.js
@@ -0,0 +1,1 @@
+import{L as n}from"./Layout.ba29727e.js";import{j as o}from"./index.387b7adb.js";function a(){return o(n,{children:o("h1",{children:"Renovation"})})}export{a as default};
--- assets/index.ec4858b3.js
+++
@@ -1,3 +0,0 @@
-import{j as o,s as t}from"./index.35023516.js";import{L as r}from"./Layout.489ca6a1.js";import"./Header.18e054c9.js";function e(){return o(r,{children:o(a,{})})}const a=t.div`
-
-`;export{e as default};
